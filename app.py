6b9e0c7385a77fb44ecd0b644119f11d4a871e59
Yousra <yousra@A610-3.local>
2022-01-13 18:05:30 +0100

    add todo & signup

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from dataclasses import dataclass
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_cors import CORS
+from datetime import datetime
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/yousra/Desktop/apps/todo-test-back/database.db'
 db = SQLAlchemy(app)
 
@@ -14,7 +17,7 @@ class User(db.Model):
         return '<User %r>' % self.email
 
 
-
+@dataclass
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -24,3 +27,34 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Todo %r>' % self.title
+
+    
+@app.route("/user", methods=["POST"])
+def signup():
+    new = User(email=request.json['email'])
+    db.session.add(new)
+    db.session.commit()
+    data = {'message': 'Created', 'code': 'SUCCESS'}
+    return make_response(jsonify(data), 201)
+
+@app.route("/todo", methods=["POST"])
+def addTodo():
+    if request.json['description'] != '':
+        new = Todo(title=request.json['title'],
+                description=request.json['description'],
+                date=datetime.strptime(request.json['date'], "%Y-%m-%d"),
+                done=False)
+    else:
+        new=Todo(title=request.json['title'],
+                description=request.json['description'],
+                done=False)
+    
+    db.session.add(new)
+    db.session.commit()
+    data = {'message': 'Created', 'code': 'SUCCESS'}
+    return make_response(jsonify(data), 201)
+
+@app.route("/todos", methods=["GET"])
+def GetTodos():
+    todos=Todo.query.all()
+    return make_response(jsonify(todos), 200)
